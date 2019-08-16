a35c8c302406f1e26980e8b70814ad4c158517ad
tyhja-mitaton <nullum0@gmail.com>
2019-08-16 17:46:31 +0500

    Migrations fix

---
+++ migrations/m190816_123140_create_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m190816_123140_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(),
+            'password' => $this->string(),
+            'role' => $this->string()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
